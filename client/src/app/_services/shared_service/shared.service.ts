bc51dbf398a28d2b7994ecc0f9806cd648b066be
Steve Gaita <steveggaita@gmail.com>
2020-09-27 15:03:39 +0300

    update shared service

--- client/src/app/_services/shared_service/shared.service.ts
+++ client/src/app/_services/shared_service/shared.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
+  private userData = new BehaviorSubject([]);
+  sharedUser = this.userData.asObservable();
+
   constructor() { }
+
+  nextUserData(user: []) {
+    this.userData.next(user)
+  }
 }
